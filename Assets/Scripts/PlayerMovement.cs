83544a435edf67107b690d0221aec2e65ce2bed5
Marius Macheret <marius.macheret@gmail.com>
2023-10-13 17:15:07 +0200

    Fix Bouncer
    Retrait de l'ancien script bouncer
    Ajout de paramètres à tweeker (et pas twerker)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using UnityEngine.Animations;
 
@@ -8,16 +10,29 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody _rb;
     private bool _isGrounded;
     private bool _isBoosted;
+    private bool _isBumping;
     private Vector3 originalVelocity;
+    private Vector3 bumperVelocity;
 
     private float yaw = 0.0f, pitch = 0.0f;
-    [SerializeField] private float _speed = 1.0f, _jumpForce, _sensitivity = 2.0f;
+    [SerializeField] private float _speed = 1.0f, _jumpForce, _sensitivity;
 
-    public float _boostSpeed;
+    private float _originalSensitivity;
 
+    public float _liquidSpeed; //multiplicateur de vitesse dans la bulle. 1 signifie que la vitesse est la même dans la bulle qu'a l'exterieur. 
+    public float _liquidLook; //Sensivité de la caméra dans la bulle (ne pas mettre une sensibilité au dessus de la sensibilité de base) 
+
+    public float _bumpForce; //puissance factorielle du bumper. 1 signifie que le bumper renvoie la même force que l'on lui donne. Valeur de base 0.9 (légère perte de puissance de vélocité à chaque saut de bumper)
+    public float _bumpInfluence; //influence de la direction du bumper sur la trajectoire du joueur. Uniquement mettre une valeur float entre 0 et 1. 0 signifie que le player rebondit comme sur un miroir, et 1 qu'il suit complètement la direction du bumper.
+    public float _bumpMIN; //Le minimum de base est de 50. Empêche le joueur de faire de touts petits sauts par erreurs.
+    public float _bumpMAX; //le maximum de base est 100. Empêche le joueur de stacker trop de vitesse après au saut. 
+    
+    private bool _debugIsFrameCollide = false; //DEBUG 
+    private Vector3 _velovityBug; //DEBUG
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _originalSensitivity = _sensitivity;
     }
 
     void Update()
@@ -27,9 +42,11 @@ public class PlayerMovement : MonoBehaviour
             _rb.velocity = new Vector3(_rb.velocity.x, _jumpForce, _rb.velocity.z);
         }
         Look();
+
+        bumperVelocity = _rb.velocity;
     }
 
-    void Look() //tuto ytb pour la souris
+    void Look() //tuto ytb pour la souris (fonctionnel)
     {
 
         pitch -= Input.GetAxisRaw("Mouse Y") * _sensitivity;
@@ -38,13 +55,18 @@ public class PlayerMovement : MonoBehaviour
         Camera.main.transform.localRotation = Quaternion.Euler(pitch, yaw, 0);
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         if (_isBoosted == false)
         {
-            Movement();
+            //Movement(); //activer ou réactiver si l'on veut tester les bumpers (ils ne fonctionnent pas quand activé). 
         }
 
+        //if (_debugIsFrameCollide == true) //Debug
+        //{
+            //Debug.Log("After Velocity: " + _rb.velocity);
+            //_debugIsFrameCollide = false;
+        //}
     }
 
     void Movement() //tuto ytb pour le mouvement (fonctionne en changant directement la vélocité) 
@@ -55,7 +77,7 @@ public class PlayerMovement : MonoBehaviour
         _rb.velocity = direction;
     }
 
-    private void OnTriggerEnter(Collider Liquid)
+    void OnTriggerEnter(Collider Liquid)
     {
         if (Liquid.gameObject.tag == "BoostLiquid")
         {
@@ -65,22 +87,41 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay(Collider Liquid)
+    void OnTriggerStay(Collider Liquid)
     {
         if (Liquid.gameObject.tag == "BoostLiquid")
         {
-            _sensitivity = Mathf.Min(_sensitivity / 1.05f, 1f); //réduit la sensi de la souris dans le liquide de boost (pas néscessaire) 
-            _rb.AddForce(_rb.velocity * _boostSpeed, ForceMode.Force); //boost de la velocité 
+            _sensitivity = Mathf.Min(_sensitivity / 1.05f, _liquidLook); //réduit la sensi de la souris dans le liquide de boost (pas néscessaire) 
+            _rb.AddForce(_rb.velocity * _liquidSpeed, ForceMode.Force); //boost de la velocité 
         }
     }
 
-    private void OnTriggerExit(Collider Liquid)
+    void OnTriggerExit(Collider Liquid)
     {
         if (Liquid.gameObject.tag == "BoostLiquid")
         {
-            _isBoosted = false;
-            _sensitivity = 2; 
+            _isBoosted = false;  
+            _sensitivity = _originalSensitivity; 
             _rb.useGravity = true; 
         }
     }
+
+    void OnCollisionEnter(Collision Bumper)
+    {
+        if (Bumper.gameObject.tag == "Bouncer")
+            {
+            var _bumpSpeed = Mathf.Clamp(bumperVelocity.magnitude * _bumpForce, _bumpMIN, _bumpMAX); //récupère la vitesse (magnitude) du player et la factorise avec la puissance que le bump va renvoyer, et défini une distance de saut de bumper Min et Max 
+            Vector3 _mirrorDirection = Vector3.Reflect(bumperVelocity.normalized, Bumper.contacts[0].normal); //Vecteur Miroir réfléchi sur la normale du bumper.
+            Vector3 _bumpDirection = Vector3.Lerp(_mirrorDirection, Bumper.contacts[0].normal, _bumpInfluence); //Définition de l'influence de la direction du bumper (max 1) sur le vecteur miroir (min 0) réfléchi dessus.
+
+            _rb.velocity = _bumpDirection * _bumpSpeed; //application de la vélocité sur le player
+
+            //_velovityBug = _bumpDirection * _bumpSpeed; //Debug
+            //Debug.Log("Bumper Direction: " + _bumpDirection);
+            //Debug.Log("Reflect Result: " + _velovityBug);
+            //Debug.Log("Final Velocity: " + _rb.velocity);
+            //_debugIsFrameCollide = true;
+        }
+    }
 }
+

3871b2b6c55620bcb8d69e2774fe64afae643009
BattleFox200 <marius.macheret@gmail.com>
2023-10-30 17:13:35 +0100

    SizeManager (WIP)
    ne fonctionne pas encore !!
    Sert à gérer la taille de la bulle et des pads selon la vitesse du joueur et de les faire grossir avec le temps.

---
+++ Assets/Scripts/SizeManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeManager : MonoBehaviour
+{
+    public float sizeRatioOnSpeed; //Mettre à 0 pour annuler la fonctionnalité
+    public float sizeRatioOnTime; //Mettre à 0 pour annuler la fonctionnalité
+
+    private Rigidbody _RB;
+
+    public BlocVanish VanishScript;
+
+    private void Start()
+    {
+        if (sizeRatioOnSpeed != 0)
+        {
+            _RB = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+            float sizeresult = transform.localScale.x + (transform.localScale.x * (sizeRatioOnSpeed * _RB.velocity.magnitude));
+            transform.localScale = new Vector3(sizeresult, sizeresult, sizeresult);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (sizeRatioOnTime != 0)
+        {
+            float sizeX = (transform.localScale.x + (transform.localScale.x * sizeRatioOnTime));
+            float sizeY = (transform.localScale.y + (transform.localScale.y * sizeRatioOnTime));
+            float sizeZ = (transform.localScale.z + (transform.localScale.z * sizeRatioOnTime));
+            transform.localScale = new Vector3(sizeX, sizeY, sizeZ);
+        }
+    }
+}

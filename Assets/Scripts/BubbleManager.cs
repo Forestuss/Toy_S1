624df865880d60e26c518257c0b193584b2304b0
Forestuss <guillaume.quertain@gmail.com>
2023-12-04 20:46:43 +0100

    Ajout de gamefeel sur les bubble
    SmoothDamp de la caméra quand a l'interieur d'une bulle

--- Assets/Scripts/BubbleManager.cs
+++ Assets/Scripts/BubbleManager.cs
@@ -1,8 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BubbleManager : MonoBehaviour
 {
     private Rigidbody rb;
+
+    private float _originalFOV = 80f;
+    private float _newFOV = 110f;
+    private float _tempoFOV;
+
+    private float velocity = 0f;
+
+    public float smoothTime = 0.5f;
+
+    public bool _inBubble = false;
 
     [SerializeField] private GameObject bubble;
     [SerializeField] private GameObject Target;
@@ -17,42 +28,65 @@ public class BubbleManager : MonoBehaviour
     [Header("Liquid")]
     public float _liquidSpeed; //multiplicateur de vitesse dans la bulle. 1 signifie que la vitesse est la même dans la bulle qu'a l'exterieur. 
     public float _liquidLook; //Sensivité de la caméra dans la bulle (ne pas mettre une sensibilité au dessus de la sensibilité de base) 
-    public float maxSizeBubble = 20;
+    [Range(20f ,50f)] public float maxSizeBubble;
+    [Range(0f, 30)] public float minSizeBubble;
+
+    public float randomSize;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
+
+        
     }
 
     void Update()
     {
+        if(_inBubble)
+        {
+            _tempoFOV = (Mathf.SmoothDamp(Camera.main.fieldOfView, _newFOV, ref velocity, smoothTime));
+            
+        }
+        else
+        {
+            _tempoFOV = (Mathf.SmoothDamp(Camera.main.fieldOfView, _originalFOV, ref velocity, smoothTime));
+           
+        }
+        Camera.main.fieldOfView = _tempoFOV;
+
         if (Input.GetMouseButtonDown(0))
         {
+            randomSize = Random.Range(minSizeBubble, maxSizeBubble);
             instanceBubble = Instantiate(bubble, Target.transform.position, Quaternion.identity);
+            
+            instanceBubble.transform.localScale = new Vector3(randomSize ,randomSize ,randomSize);
         }
 
-        /*if (Input.GetMouseButtonDown(0))
-        {
-            instanceBubble = Instantiate(bubble, Target.transform.position, Quaternion.identity, player.transform);
-        }
 
-        if (Input.GetMouseButton(0) && instanceBubble.transform.localScale.x < maxSizeBubble)
-        {
-            instanceBubble.transform.localScale = instanceBubble.transform.localScale + new Vector3(0.1f, 0.1f, 0.1f);
-        }
 
-        if (Input.GetMouseButton(0) && instanceBubble.transform.localScale.x < maxSizeBubble)
-        {
-            instanceBubble.transform.rotation = Camera.main.transform.rotation;
-            instanceBubble.transform.position = Target.transform.position;
-        }
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    instanceBubble = Instantiate(bubble, Target.transform.position, Quaternion.identity, player.transform);
+        //}
+
+        //if (Input.GetMouseButton(0) && instanceBubble.transform.localScale.x < maxSizeBubble)
+        //{
+        //    instanceBubble.transform.localScale = instanceBubble.transform.localScale + new Vector3(0.1f, 0.1f, 0.1f);
+        //}
+
+        //if (Input.GetMouseButton(0) && instanceBubble.transform.localScale.x < maxSizeBubble)
+        //{
+        //    instanceBubble.transform.rotation = Camera.main.transform.rotation;
+        //    instanceBubble.transform.position = Target.transform.position;
+        //}
         
-        if (Input.GetMouseButtonUp(0))
-        {
-            instanceBubble.transform.SetParent(null);
-        }*/
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    instanceBubble.transform.SetParent(null);
+        //}
     }
     void OnTriggerEnter(Collider Liquid)
     {
@@ -61,6 +95,7 @@ public class BubbleManager : MonoBehaviour
             rb.useGravity = false; //enlève la gravité dans le liquide de boost 
             //isBoosted = true; // annule la possibilité de mouvement 
             originalVelocity = rb.velocity;
+            _inBubble = true;
         }
     }
 
@@ -79,6 +114,7 @@ public class BubbleManager : MonoBehaviour
         {
             //isBoosted = false;
             rb.useGravity = true;
+            _inBubble = false;
         }
     }
 }

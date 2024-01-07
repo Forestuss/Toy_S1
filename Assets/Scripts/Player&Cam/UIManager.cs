b7be47bc56acbeaee23f45871b39ffb7657f2ab2
Forestuss <guillaume.quertain@gmail.com>
2024-01-07 21:27:51 +0100

    lerp rota UI

--- Assets/Scripts/Player&Cam/UIManager.cs
+++ Assets/Scripts/Player&Cam/UIManager.cs
@@ -6,12 +6,14 @@ public class UIManager : MonoBehaviour
 {
     private Transform _cameraTransform;
 
+    [SerializeField] private float _smoothTime = 0.01f;
+
     private void Start()
     {
         _cameraTransform = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
     }
     private void Update()
     {
-        transform.rotation = _cameraTransform.rotation;
+        transform.rotation = Quaternion.Lerp(transform.rotation, _cameraTransform.rotation, _smoothTime);
     }
 }

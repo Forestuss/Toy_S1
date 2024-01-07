9310d4b3318507ba124cf6fdb2eb6e3a2aff327e
JonathanGautreau <124163841+JonathanGautreau@users.noreply.github.com>
2024-01-07 12:33:33 +0100

    puleslide

--- Assets/Scripts/SonCollision.cs
+++ Assets/Scripts/SonCollision.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class SonCollision : MonoBehaviour
 {
+    //
     private FMOD.Studio.EventInstance SonHit;
     private FMOD.Studio.EventInstance SonUseRing;
     private FMOD.Studio.EventInstance Music;
     private FMOD.Studio.EventInstance PiedMarche;
-    private FMOD.Studio.EventInstance Slide;
+
     private Rigidbody rb;
     private float velocity;
     private PlayerMovements movementscript;
@@ -22,9 +23,9 @@ public class SonCollision : MonoBehaviour
         SonUseRing = FMODUnity.RuntimeManager.CreateInstance("event:/RingBehave/UseRing");
         Music = FMODUnity.RuntimeManager.CreateInstance("event:/WorldBehave/Ambiance");
         PiedMarche = FMODUnity.RuntimeManager.CreateInstance("event:/PlayerBehave/MoveOnFloor");
-        Slide = FMODUnity.RuntimeManager.CreateInstance("event:/PlayerBehave/Slide");
+
         Music.start();
-        Slide.start();  
+
         rb = GetComponent<Rigidbody>();
         movementscript = GetComponent<PlayerMovements>();
         
@@ -34,17 +35,8 @@ public class SonCollision : MonoBehaviour
     void Update()
     {
         Music.setParameterByName("Hauteur", transform.position.y-250f);
-        if (rb.velocity.magnitude>= 60.0f && movementscript.isGrounded)
-        {
-            Slide.setParameterByName("Volume", 1);
-        }
-        else
-        {
-            Slide.setParameterByName("Volume", 0);
-        }
-        Slide.setParameterByName("Vitesse", rb.velocity.magnitude);
         velocity = rb.velocity.magnitude;
-
+;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -84,14 +76,14 @@ public class SonCollision : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             PiedMarche.setParameterByName("TypeSol", 1);
-            Slide.setParameterByName("TypeSol", 1);
+
 
 
         }
         else if (collision.gameObject.CompareTag("Wall"))
         {
             PiedMarche.setParameterByName("TypeSol", 1);
-            Slide.setParameterByName("TypeSol", 1);
+
         }
     }
     private void OnTriggerEnter(Collider other)

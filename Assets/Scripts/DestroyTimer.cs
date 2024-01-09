cbb3c378380f0cead9e721b4a4a449bdfdd4a878
Forestuss <guillaume.quertain@gmail.com>
2024-01-09 10:53:32 +0100

    comment script

--- Assets/Scripts/DestroyTimer.cs
+++ Assets/Scripts/DestroyTimer.cs
@@ -10,7 +10,7 @@ public class DestroyTimer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tempsAvantDestruct = tempsAvantDestruct * 60f;
+        tempsAvantDestruct = tempsAvantDestruct * 60f; // converti le temps reseigné en minutes
         tempsDestruct = tempsAvantDestruct;
     }
 
